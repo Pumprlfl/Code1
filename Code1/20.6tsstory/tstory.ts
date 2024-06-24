cda0bdb3e1d618c559daa905232ac8283d531d25
Pumprlfl <christian.friedrich.bauer04@gmail.com>
2024-06-24 12:14:30 +0200

    po

--- Code1/20.6tsstory/tstory.ts
+++ Code1/20.6tsstory/tstory.ts
@@ -1,8 +1,14 @@
-let Beginning: string = prompt("Having lent ...")!;
+let s = ["Having lent ...", 
+    "arriving...",
+    "opening the door...",
+    "walking through...",
+    "your cousin catches you not working and eats you"
+]
+let Beginning: string = prompt(s[0])!;
 
 switch (Beginning) {
     case "left":
-        let Answerleft: string=prompt ("arriving...")!;
+        let Answerleft: string=prompt (s[1])!;
         switch (Answerleft){
             case "take":
                 alert("death")
@@ -12,12 +18,12 @@ switch (Beginning) {
             break;}
         break;
     case "right":
-        let Answerright: string=prompt ("opening the door...")!;
+        let Answerright: string=prompt (s[2])!;
         break;
     case "forward":
-        let Answerforward: string=prompt ("walking through...")!;
+        let Answerforward: string=prompt (s[3])!;
         break;
     default:
-        alert("your cousin catches you not working and eats you")
+        alert(s[4])
     break;
    }
