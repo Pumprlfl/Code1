8439385cba93c1878d0e6e30db8f326971ab666a
Pumprlfl <christian.friedrich.bauer04@gmail.com>
2024-06-25 11:04:24 +0200

    25.2

--- Code1/Dommanipul/Dommaa.js
+++ Code1/Dommanipul/Dommaa.js
@@ -1,3 +1,7 @@
 "use strict";
 let spanElement = document.getElementsByTagName("span")[0];
 spanElement.textContent = "Aaahh";
+let newspan = document.createElement("span");
+newspan.textContent = "World";
+document.body.appendChild(newspan);
+console.log(newspan);
--- Code1/Dommanipul/Dommaa.ts
+++ Code1/Dommanipul/Dommaa.ts
@@ -1,2 +1,8 @@
 let spanElement = document.getElementsByTagName("span")[0];
 spanElement.textContent = "Aaahh";
+
+let newspan = document.createElement("span");
+newspan.textContent = "World";
+document.body.appendChild(newspan);
+
+console.log(newspan);
